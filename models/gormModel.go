6f497779de637953404663d20e8f9fbfb50be263
atha <athabarokahfc@gmail.com>
2024-03-23 11:17:32 +0700

    remove: deleted_at

--- models/gormModel.go
+++ models/gormModel.go
@@ -2,13 +2,10 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type GormModel struct {
     ID        uint           `gorm:"primary_key;autoIncrement" json:"id"`
     CreatedAt time.Time
     UpdatedAt time.Time
-    DeletedAt gorm.DeletedAt `gorm:"index"`
 }
